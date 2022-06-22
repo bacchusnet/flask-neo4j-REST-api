c0c0db70ade5e55ea56077b763535d6977e8afe0
yoreee <rerebacchus@gmail.com>
2022-06-22 03:36:58 +0000

    first commit

---
+++ api/dao/genres.py
@@ -0,0 +1,75 @@
+from api.data import genres
+from api.exceptions.notfound import NotFoundException
+
+class GenreDAO:
+    """
+    The constructor expects an instance of the Neo4j Driver, which will be
+    used to interact with Neo4j.
+    """
+    def __init__(self, driver):
+        # import ipdb; ipdb.set_trace()
+
+        self.driver=driver
+
+    """
+    This method should return a list of genres from the database with a
+    `name` property, `movies` which is the count of the incoming `IN_GENRE`
+    relationships and a `poster` property to be used as a background.
+
+    [
+       {
+        name: 'Action',
+        movies: 1545,
+        poster: 'https://image.tmdb.org/t/p/w440_and_h660_face/qJ2tW6WMUDux911r6m7haRef0WH.jpg'
+       }, ...
+
+    ]
+    """
+    # tag::all[]
+    def all(self):
+    # Define a unit of work to Get a list of Genres
+        def get_movies(tx):
+            result = tx.run("""
+                MATCH (g:Genre)
+                WHERE g.name <> '(no genres listed)'
+
+                CALL {
+                    WITH g
+                    MATCH (g)<-[:IN_GENRE]-(m:Movie)
+                    WHERE m.imdbRating IS NOT NULL AND m.poster IS NOT NULL
+                    RETURN m.poster AS poster
+                    ORDER BY m.imdbRating DESC LIMIT 1
+                }
+
+                RETURN g {
+                    .*,
+                    movies: size((g)<-[:IN_GENRE]-(:Movie)),
+                    poster: poster
+                } AS genre
+                ORDER BY g.name ASC
+            """)
+
+            return [ g.value(0) for g in result ]
+
+        # Open a new session
+        with self.driver.session() as session:
+            # Execute within a Read Transaction
+            # import ipdb; ipdb.set_trace()
+            return session.read_transaction(get_movies)
+    # end::all[]
+
+
+    """
+    This method should find a Genre node by its name and return a set of properties
+    along with a `poster` image and `movies` count.
+
+    If the genre is not found, a NotFoundError should be thrown.
+    """
+    # tag::find[]
+    def find(self, name):
+        # TODO: Open a new session
+        # TODO: Define a unit of work to find the genre by it's name
+        # TODO: Execute within a Read Transaction
+
+        return [g for g in genres if g["name"] == name][0]
+    # end::find[]
---
+++ api/dao/ratings.py
@@ -0,0 +1,65 @@
+from api.data import ratings
+from api.exceptions.notfound import NotFoundException
+
+from api.data import goodfellas
+
+
+class RatingDAO:
+    """
+    The constructor expects an instance of the Neo4j Driver, which will be
+    used to interact with Neo4j.
+    """
+    def __init__(self, driver):
+        self.driver=driver
+
+    """
+    Add a relationship between a User and Movie with a `rating` property.
+    The `rating` parameter should be converted to a Neo4j Integer.
+    """
+    # tag::add[]
+    def add(self, user_id, movie_id, rating):
+        # TODO: Create function to save the rating in the database
+        # TODO: Call the function within a write transaction
+        # TODO: Return movie details along with a rating
+        def create_rating(tx, user_id, movie_id, rating):
+            return tx.run(""" 
+            MATCH (u:User {userId: $user_id})
+            MATCH (m:Movie {tmdbId: $movie_id})
+
+            MERGE (u)-[r:RATED]->(m)
+            SET r.rating = $rating,
+                r.timestampe = timestamp()
+            
+            return m {
+                .*,
+                rating: r.rating
+            } as movie
+            """, user_id=user_id, movie_id=movie_id, rating=rating).single()
+
+        with self.driver.session() as session:
+            record = session.write_transaction(create_rating, user_id=user_id, movie_id=movie_id, rating=rating)
+
+            if record is None:
+                raise NotFoundException()
+
+            return record["movie"]
+
+        
+    # end::add[]
+
+
+    """
+    Return a paginated list of reviews for a Movie.
+
+    Results should be ordered by the `sort` parameter, and in the direction specified
+    in the `order` parameter.
+    Results should be limited to the number passed as `limit`.
+    The `skip` variable should be used to skip a certain number of rows.
+    """
+    # tag::forMovie[]
+    def for_movie(self, id, sort = 'timestamp', order = 'ASC', limit = 6, skip = 0):
+        # TODO: Get ratings for a Movie
+        # TODO: Remember to escape the braces in the cypher query with double braces: {{ }}
+
+        return ratings
+    # end::forMovie[]
